6984996c2bc4143b240a185823179131a9e4f252
Benjamin <benjamin.d.chin@gmail.com>
2024-04-19 01:02:06 -0400

    rose-pine

--- lua/ben/plugins/colorscheme.lua
+++ lua/ben/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
---    'rose-pine/neovim',
---    name = 'rose-pine',
---
---    config = function()
---        require('rose-pine').setup({
---            variant = 'main',
---            dim_inactive_windows = true,
---            extend_background_behind_borders = true,
---
---            styles = {
---                bold = true,
---                italic = true,
---                transparency = true,
----            },
---        })
---        vim.cmd('colorscheme rose-pine')
---    end
+    'rose-pine/neovim',
+    name = 'rose-pine',
+
+    config = function()
+        require('rose-pine').setup({
+            variant = 'main',
+            dim_inactive_windows = true,
+            extend_background_behind_borders = true,
+
+            styles = {
+                bold = true,
+                italic = true,
+                transparency = true,
+            },
+        })
+        vim.cmd('colorscheme rose-pine')
+    end
 }
